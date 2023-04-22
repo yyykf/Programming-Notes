ac734fcc15e730a1a84d373effac7edeab720cda
YuKaiFan <yukaifan@code4j.site>
2023-04-22 14:04:15 +0800

    fix(): update umami tracking code

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -4,29 +4,16 @@ import theme from "./theme";
 
 export default defineUserConfig({
     head: [
-        // 百度统计
+        // Umami tracking
         [
-            "script", {},
-            `var _hmt = _hmt || [];
-            (function() {
-              var hm = document.createElement("script");
-              hm.src = "https://hm.baidu.com/hm.js?50fd97c48b060fbbcfa0a11c4257b817";
-              var s = document.getElementsByTagName("script")[0]; 
-              s.parentNode.insertBefore(hm, s);
-            })();`
-        ],
-        // umami
-        [
-            "script", {},
-            `var _hmt = _hmt || [];
-            (function() {
-              var hm = document.createElement("script");
-              hm.src = "https://umami.code4j.site/script.js";
-              hm.data-website-id="41664a06-4383-4a06-bc87-2e39d03c6661"
-              var s = document.getElementsByTagName("script")[0]; 
-              s.parentNode.insertBefore(hm, s);
-            })();`
-        ],
+          'script',
+          {
+            async: true,
+            defer: true,
+            'data-website-id': '41664a06-4383-4a06-bc87-2e39d03c6661',
+            src: 'https://umami.code4j.site/script.js'
+          }
+        ]
     ],
     theme,
     // 是否开启默认预加载 js
